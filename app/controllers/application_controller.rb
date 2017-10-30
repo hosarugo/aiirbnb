144ed3818e25cc1eeea55b63964aca8b6ec8454e
hosarugo <hosa.rugo@gmail.com>
2017-10-30 22:29:09 +0000

    creation page utilisateur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname,:avatr])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname,:avatr, :phone_number, :description])
   end
 
 end
