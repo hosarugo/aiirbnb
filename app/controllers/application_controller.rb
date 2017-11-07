33344c92c1a26116d5a3463d4efc50fcb6cb1889
hosarugo <hosa.rugo@gmail.com>
2017-11-07 15:19:12 +0000

    avancement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname,:avatr, :phone_number, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname,:avatar, :phone_number, :description])
   end
 
 end
